2864a88d4e45632308a86c23c9c44a99c76b92f3
seanimayi <seanimayi@gmail.com>
2026-03-18 20:42:45 +0100

    Health done

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,11 @@
 import { Controller, Get, Route, Tags } from "tsoa";
-import { Adullam } from "../../services/BankService.js";
 @Route("adullam")
 @Tags("Adullam")
 export class UsersController extends Controller {
-  
+  @Get("/health")
+  public static async Health() {
+    return {
+        message: 'Health'
+    }
+  }
 }
